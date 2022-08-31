296afde327cb284057c3329870902f5a0c789637
Logan Olson <loganolsonn@gmail.com>
2022-08-30 21:38:08 -0500

    Moved Countries off the home page

---
+++ src/pages/countries.tsx
@@ -0,0 +1,41 @@
+import type { NextPage } from 'next';
+import React from 'react';
+import { trpc } from '../utils/trpc';
+import CountryCard from '../components/CountryCard';
+
+const CountriesPage: NextPage = () => {
+  const { data, isLoading } = trpc.useQuery(['country.getAllCountries']);
+  const [inputText, setInputText] = React.useState<string>('');
+
+  const handleSearch = (event: { target: { value: string } }) => {
+    setInputText(event.target.value);
+  };
+
+  if (isLoading || !data) return <div>Loading.....</div>;
+  return (
+    <div className="p-6 min-h-screen w-screen items-stretch relative">
+      <input
+        onChange={(e) => {
+          handleSearch(e);
+        }}
+        placeholder="Search for Country"
+      ></input>
+      <div className="grid grid-cols-1 gap-y-5 md:grid-cols-4 md:gap-x-5 mt-10">
+        {inputText === ''
+          ? data?.map((country) => (
+              <CountryCard country={country} key={country.id} />
+            ))
+          : data
+              ?.filter((country) => country.name.includes(inputText))
+              .map((filteredCountry) => (
+                <CountryCard
+                  country={filteredCountry}
+                  key={filteredCountry.id}
+                />
+              ))}
+      </div>
+    </div>
+  );
+};
+
+export default CountriesPage;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,43 +1,8 @@
 import type { NextPage } from 'next';
-import React, { useEffect } from 'react';
-import Head from 'next/head';
-import { trpc } from '../utils/trpc';
-import { Country } from '@prisma/client';
-import CountryCard from '../components/CountryCard';
+import React from 'react';
 
 const Home: NextPage = () => {
-  const { data, isLoading } = trpc.useQuery(['country.getAllCountries']);
-  const [inputText, setInputText] = React.useState<string>('');
-
-  const handleSearch = (event: { target: { value: string } }) => {
-    setInputText(event.target.value);
-  };
-
-  if (isLoading || !data) return <div>Loading.....</div>;
-  return (
-    <div className="p-6 min-h-screen w-screen items-stretch relative">
-      <input
-        onChange={(e) => {
-          handleSearch(e);
-        }}
-        placeholder="Search for Country"
-      ></input>
-      <div className="grid grid-cols-1 gap-y-5 md:grid-cols-4 md:gap-x-5 mt-10">
-        {inputText === ''
-          ? data?.map((country) => (
-              <CountryCard country={country} key={country.id} />
-            ))
-          : data
-              ?.filter((country) => country.name.includes(inputText))
-              .map((filteredCountry) => (
-                <CountryCard
-                  country={filteredCountry}
-                  key={filteredCountry.id}
-                />
-              ))}
-      </div>
-    </div>
-  );
+  return <div>Hi</div>;
 };
 
 export default Home;
